d7bed6755dc7900c37a4837c2ceab5f266966588
celeania <iambachelorfrog@gmail.com>
2015-07-16 11:18:44 -0700

    Create parallaxes.js

---
+++ parallaxes.js
@@ -0,0 +1,18 @@
+function parallaxBackground(){
+// bg parallax animations
+ $(".bg").interactive_bg({
+   strength: 30,              // Movement Strength when the cursor is moved. The higher, the faster it will reacts to your cursor. The default value is 25.
+   scale: 1,               // The scale in which the background will be zoomed when hovering. Change this to 1 to stop scaling. The default value is 1.05.
+   animationSpeed: "200ms",   // The time it takes for the scale to animate. This accepts CSS3 time function such as "100ms", "2.5s", etc. The default value is "100ms".
+   contain: true,             // This option will prevent the scaled object/background from spilling out of its container. Keep this true for interactive background. Set it to false if you want to make an interactive object instead of a background. The default value is true.
+   wrapContent: false         // This option let you choose whether you want everything inside to reacts to your cursor, or just the background. Toggle it to true to have every elements inside reacts the same way. The default value is false
+ });
+ 
+ // change background size on window resize
+ $(window).resize(function() {
+    $(".bg > .ibg-bg").css({
+      width: $(window).outerWidth(),
+     height: $(window).outerHeight()
+  })
+ })
+}
